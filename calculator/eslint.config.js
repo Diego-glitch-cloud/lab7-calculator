ce6dc24ec7154a70341b4de04b7beb74be9360e4
Diego-glitch-cloud <dcalderon918@gmail.com>
2026-05-23 21:22:38 -0600

    build: configuracion del storybook

--- calculator/eslint.config.js
+++ calculator/eslint.config.js
@@ -1,49 +1,49 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended
-    ],
-    files: ['**/*.{ts,tsx,js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      // Standard-like rules
-      'semi': ['error', 'never'],
-      'max-len': ['error', { 'code': 120 }],
-      'indent': ['error', 2],
-      'quotes': ['error', 'single'],
-      'comma-spacing': ['error', { 'before': false, 'after': true }],
-      'keyword-spacing': ['error', { 'before': true, 'after': true }],
-      'space-before-function-paren': ['error', 'always'],
-      'space-infix-ops': 'error',
-      'no-multi-spaces': 'error',
-      'space-in-parens': ['error', 'never'],
-      'array-bracket-spacing': ['error', 'never'],
-      'object-curly-spacing': ['error', 'always'],
-      'arrow-spacing': ['error', { 'before': true, 'after': true }],
-      'no-trailing-spaces': 'error',
-      'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 0 }],
-      'no-unused-vars': 'off', // handled by tseslint
-      '@typescript-eslint/no-unused-vars': ['warn']
-    },
+export default tseslint.config({ ignores: ['dist'] }, {
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.recommended
+  ],
+  files: ['**/*.{ts,tsx,js,jsx}'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    globals: globals.browser,
   },
-)
+  plugins: {
+    'react-hooks': reactHooks,
+    'react-refresh': reactRefresh,
+  },
+  rules: {
+    ...reactHooks.configs.recommended.rules,
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    // Standard-like rules
+    'semi': ['error', 'never'],
+    'max-len': ['error', { 'code': 120 }],
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'comma-spacing': ['error', { 'before': false, 'after': true }],
+    'keyword-spacing': ['error', { 'before': true, 'after': true }],
+    'space-before-function-paren': ['error', 'always'],
+    'space-infix-ops': 'error',
+    'no-multi-spaces': 'error',
+    'space-in-parens': ['error', 'never'],
+    'array-bracket-spacing': ['error', 'never'],
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { 'before': true, 'after': true }],
+    'no-trailing-spaces': 'error',
+    'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 0 }],
+    'no-unused-vars': 'off', // handled by tseslint
+    '@typescript-eslint/no-unused-vars': ['warn']
+  },
+}, storybook.configs["flat/recommended"]);
